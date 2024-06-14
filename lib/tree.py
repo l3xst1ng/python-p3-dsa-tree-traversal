59dad8557f7535f4130ecb5b802a561cbae7ddbf
l3xst1ng <howdy.martt@gmail.com>
2024-06-14 16:15:09 +0300

    Completed python-p3-dsa-tree-traversal lab

--- lib/tree.py
+++ lib/tree.py
@@ -6,20 +6,20 @@ class Tree:
         if self.root is None:
             return None
 
-        # Create a list to store the nodes to visit
+        # List to store the nodes to visit
         nodes_to_visit = [self.root]
 
-        # Traverse the tree
+        # Traversing the tree
         while nodes_to_visit:
             # Get the next node to visit
             node = nodes_to_visit.pop(0)
 
-            # Check if the node's ID matches the desired ID
+            # Checking if the node's ID matches the desired ID
             if node['id'] == id:
                 return node
 
-            # Add the node's children to the list of nodes to visit
+            # Adding the node's children to the list of nodes to visit
             nodes_to_visit.extend(node['children'])
 
-        # If the desired node is not found, return None
+        # Returning None if the desired node isn't found
         return None
